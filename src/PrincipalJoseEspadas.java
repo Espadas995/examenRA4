a29cbc23403e71916305a3f767d64eeb0561be26
Espadas995 <joseespadas995@gmail.com>
2025-05-08 16:08:54 +0200

    Commit inicial

---
+++ src/PrincipalJoseEspadas.java
@@ -0,0 +1,7 @@
+public class PrincipalJoseEspadas {
+    public static void main(String[] args) {
+        ImprimirJoseEspadas miImprimirJoseEspadas = new ImprimirJoseEspadas();
+
+        miImprimirJoseEspadas.imprimirDatosJulioHornos("Julio", "Hornos", "16807523H", "Soria");
+    }
+}
